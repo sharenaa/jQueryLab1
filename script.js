108460ab1e8b3cbc44292e5e36b7ba3f787a7707
sharena <144714984+sharenaa@users.noreply.github.com>
2025-12-22 22:46:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+function hideAll() {
+  $(".plan").fadeOut(300);
+}
 
+$("#missionBtn").click(function(e) {
+  e.preventDefault();
+  hideAll();
+
+  //  colors for heading and style
+  $("#mission h1").text("Our Mission").css("color", "#89CE9A");
+
+  $("#mission").delay(300).fadeIn(800);
+});
+
+$("#whyBtn").click(function(e) {
+  e.preventDefault();
+  hideAll();
+
+  $("#why h1").text("Why We Build").css("color", "#66AAEE");
+
+  $("#why").delay(300).slideDown(700);
+});
+
+$("#advocacyBtn").click(function(e) {
+  e.preventDefault();
+  hideAll();
+
+  $("#advocacy h1").text("Advocacy in Action");
+  $("#advocacy h1").css({
+    "color": "#555",
+    "font-size": "36px"
+  });
+
+  $("#advocacy").delay(300).fadeIn(600);
+});
